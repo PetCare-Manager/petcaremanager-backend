d05cdf035123b327437e332067298329ddfd4d45
Diego Guaman <diegoguaman05@hotmail.com>
2024-10-13 16:34:50 +0200

    Add the swagger file for documentation

--- src/framework/routes/router.ts
+++ src/framework/routes/router.ts
@@ -1,8 +1,11 @@
 import { Express } from 'express';
 import monitRouter from '../monit/monit';
+import { setupSwagger } from '../../infrastructure/config/swagger';
 
 export const RouterBuilder = (app: Express) => {
     app.use('/api/monit', monitRouter);
+
+    setupSwagger(app);
 };
 
 export default { RouterBuilder };
